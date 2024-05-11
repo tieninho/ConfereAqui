4cfa53e42619f1e15fcf36c6b52188e01c680488
tieninho <40677661+tieninho@users.noreply.github.com>
2024-05-11 18:08:42 -0300

    Update confereaqui.py

--- confereaqui.py
+++ confereaqui.py
@@ -21,7 +21,7 @@ safety_settings = {
 st.set_page_config(page_title='ConfereAqui', page_icon='🔍', layout='wide')
 
 # Fazendo o display do título da página
-st.image('LogoConfereAqui.png', width=500)
+st.image('LogoConfereAqui.png', width=400)
 
 # Texto de introdução e instrução de utilização
 st.markdown("""
