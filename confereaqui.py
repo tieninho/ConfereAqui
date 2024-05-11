f894ca2bf79d22b476fb5af515fa462cf8d3678a
tieninho <40677661+tieninho@users.noreply.github.com>
2024-05-11 19:45:57 -0300

    Update confereaqui.py

--- confereaqui.py
+++ confereaqui.py
@@ -94,7 +94,7 @@ if st.button("Verificar Notícia"):
         response.resolve()
         
         # Imprimir o texto gerado pelo modelo com a classificação da resposta
-        resposta_texto = f"**Resposta {st.session_state.resposta_counter}:** {response.text}"
+        resposta_texto = f"**Resposta {st.session_state.resposta_counter}:** \n {response.text}"
 
         # Adicionar a resposta ao histórico
         st.session_state.historico_respostas.append(resposta_texto)
