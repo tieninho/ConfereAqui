347f95d6964300876ed178e6fa1e50d16a0f1d55
tieninho <40677661+tieninho@users.noreply.github.com>
2024-05-11 17:26:55 -0300

    confereaqui.py

--- confereaqui.py
+++ confereaqui.py
@@ -100,7 +100,7 @@ if st.button("Verificar Notícia"):
 
 # Exibir histórico de respostas
 if st.session_state.historico_respostas:
-    st.subheader("🔍🔍:")
+    st.subheader("Conferi aqui 🔍")
     for resposta in st.session_state.historico_respostas:
         st.write(resposta)
         st.markdown("---")  # Linha divisória entre as respostas
